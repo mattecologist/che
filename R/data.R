d28c466ed081fa2a38950bee8d42d0b47ba0fc2c
Matt Hill <hil32c@batman-bm.nexus.csiro.au>
2019-11-13 11:43:15 +1100

    added Insect distribution data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Distribution data for 22 globally invasive insects species
+#'
+#' A dataset containing the native and invasive distributions for 22 insect species
+#'
+#' @format A data frame with 11554 rows and 4 variables:
+#' \describe{
+#'   \item{Species}{Species name}
+#'   \item{Longitude}{Longitude in WGS84}
+#'   \item{Latitiude}{Latitude in WGS84}
+#'   \item{Range}{Points are from native or invasive range}
+#'   ...
+#' }
+#' @source \url{https://doi.org/10.6084/m9.figshare.4537922.v2}
+"insect_dist"
